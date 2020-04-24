6df20f6fdcf04092afc521a92b9d97ea0f82f200
vkhanna92 <vkhanna92@tamu.edu>
2020-04-23 22:19:38 -0500

    Adding dynamodb to flask

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template, request, redirect
+import boto3
+from boto3.dynamodb.conditions import Key
 from info import APP_KEY, APP_SECRET, REDIRECT_URI
 import requests as r
 
 app = Flask(__name__)
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('CloudUsers')
 
 @app.route("/")
 def home():
@@ -29,9 +34,31 @@ def profile():
        
         access_token = resp.json()['access_token']
         account_id = resp.json()['account_id']
-
+        response = table.get_item(
+            Key={
+                'accountId': account_id
+            }
+        )
         phone_number = None
-        
+        if 'Item' in response:
+            phone_number = response['Item']['phoneNumber']
+            table.update_item(
+                Key={
+                    'accountId': account_id
+                },
+                UpdateExpression='SET accessKey = :key' ,
+                ExpressionAttributeValues={
+                    ':key': access_token
+                }
+            )
+        else:
+            table.put_item(
+                Item={
+                    'accountId' : account_id,
+                    'accessKey' : access_token
+                }
+            )
+
         return render_template('profile.html',
                                 account_id=account_id,
                                 phone_number=phone_number)
@@ -44,6 +71,15 @@ def save():
     account_id = request.args.get('account_id')
 
     if phone_number:
+        table.update_item(
+            Key={
+                'accountId' : account_id
+            },
+            UpdateExpression='SET phoneNumber = :number' ,
+            ExpressionAttributeValues={
+                ':number' : phone_number
+            }
+        )
 
         return render_template('profile.html',
                                 saved=True,
