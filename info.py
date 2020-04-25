524a059b6b378f0e5309b861580476ac2d684cba
aninditamishra <43079949+aninditamishra@users.noreply.github.com>
2020-04-24 20:03:26 -0500

    removed ARN value

--- info.py
+++ info.py
@@ -1,4 +1,3 @@
 APP_KEY = "zixl6vkeqlxtxfk" #"0tthxgsnr4af0gy"
 APP_SECRET = "z9r2nhvzer9tzsa" #"j90olfrido50ngp"
 REDIRECT_URI = "http://127.0.0.1:5000/profile"
-TOPIC_ARN = "arn:aws:sns:us-east-1:637492989301:dynamodb"
